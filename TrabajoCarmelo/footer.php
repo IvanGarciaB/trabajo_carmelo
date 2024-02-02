6f8806db876c9d1939c00789c3ad8fcec7bcddfb
usuario <user@gmail.com>
2024-02-02 10:25:34 +0100

    footer

--- TrabajoCarmelo/footer.php
+++ TrabajoCarmelo/footer.php
@@ -10,11 +10,4 @@
 
 <!-- Ejemplo de uso en una página específica -->
 
-<?php include('header.php'); ?>
 
-<!-- Contenido específico de la página -->
-
-<h1>Bienvenido a mi página</h1>
-<p>Contenido de la página...</p>
-
-<?php include('footer.php'); ?>
